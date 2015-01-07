d2eff19ba4ed0e092ed67c0858d3d0273c0d7a37
CTurt <cturt@users.noreply.github.com>
2015-01-07 22:23:52 +0000

    CRCFix support FIFA 07

--- CRCFix/source/main.c
+++ CRCFix/source/main.c
@@ -1,27 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
 
-struct fifa08save {
-	union {
-		unsigned char data[65536];
-		
-		struct {
-			uint32_t magic;
-			unsigned short crc;
-		};
-	};
+struct game {
+	char name[16];
+	unsigned int magic;
+	unsigned int crcOffset;
+	unsigned short (*crcCalculation)(unsigned char *data);
 };
 
-struct fifa08save save;
+unsigned short fifa07ecrc(unsigned char *data);
+unsigned short fifa08ecrc(unsigned char *data);
 
-unsigned short calculateCRC(unsigned char *data) {
+struct game games[] = {
+	{
+		name: "Fifa 07 E",
+		magic: 0xF1FA07BD,
+		crcOffset: 0x0A,
+		crcCalculation: fifa07ecrc,
+	},
+	
+	{
+		name: "Fifa 08 E",
+		magic: 0x10071981,
+		crcOffset: 0x04,
+		crcCalculation: fifa08ecrc,
+	}
+};
+
+unsigned short fifa07ecrc(unsigned char *data) {
+	unsigned short crc = 24;
+	int m = 0x0000026C;
+	
+	int i;
+	for(i = 0x00000028; i < 0x0000026B; i++) {
+		crc += (data[i] * ((m - (((i / 0x10) * 0x10))) - (((i / 0x4) * 0x4) - ((i / 0x10) * 0x10))));
+	}
+	
+	return crc;
+}
+
+unsigned short fifa08ecrc(unsigned char *data) {
 	unsigned short crc = 18723;
-	int m = 0x000002c4;
+	int m = 0x000002C4;
 	
 	int i;
 	for(i = 0x00000044; i < 0x00000500; i++) {
-		crc += (data[i] * ((m - (((i / 0x00000010) * 0x00000010))) - (((i / 4) * 4) - ((i / 0x00000010) * 0x00000010))));
+		crc += (data[i] * ((m - (((i / 0x10) * 0x10))) - (((i / 0x4) * 0x4) - ((i / 0x10) * 0x10))));
 	}
 	
 	return crc;
@@ -31,7 +57,7 @@ int main(int argc, char **argv) {
 	bool askBeforeFixing = true;
 	
 	if(argc < 2) {
-		printf("FIFA 08 DS Save CRC fixer\n");
+		printf("FIFA DS save CRC fixer\n");
 		printf("Usage:\n");
 		printf("CRCFix save.sav [-f]\n");
 		printf(" -f: fix CRC without asking confirmation\n");
@@ -44,7 +70,7 @@ int main(int argc, char **argv) {
 	
 	printf("File: %s\n", argv[1]);
 	
-	FILE *f = fopen(argv[1], "rb");
+	FILE *f = fopen(argv[1], "r+wb");
 	
 	if(!f) {
 		printf("Failed to open\n");
@@ -52,39 +78,57 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 	
-	fread(save.data, sizeof(struct fifa08save), 1, f);
+	unsigned int magic;
+	fread(&magic, sizeof(unsigned int), 1, f);
 	
-	fclose(f);
+	int i;
+	for(i = 0; i < sizeof(games) / sizeof(struct game); i++) {
+		if(magic == games[i].magic) {
+			break;
+		}
+		else if(i == sizeof(games) / sizeof(struct game) - 1) {
+			printf("Unrecognised game save\n");
+			printf("(magic %p)\n", magic);
+			fclose(f);
+			return 1;
+		}
+	}
 	
-	if(save.magic != 0x10071981) {
-		printf("Not a valid FIFA 08 EU save file\n");
+	printf("%s save file detected\n", games[i].name);
+	
+	fseek(f, 0, SEEK_END);
+	size_t length = ftell(f);
+	unsigned char *data = malloc(length);
+	
+	if(!data) {
+		printf("Couldn't allocate %d bytes\n", length);
+		fclose(f);
 		return 1;
 	}
 	
-	printf("Current CRC   : 0x%p\n", save.crc);
+	rewind(f);
+	fread(data, length, 1, f);
 	
-	unsigned short crc = calculateCRC(save.data);
+	unsigned short currentCrc = *(unsigned short *)(data + games[i].crcOffset);
 	
-	printf("Calculated CRC: 0x%p\n", crc);
+	printf("Current CRC    : 0x%p\n", currentCrc);
 	
-	if(save.crc != crc) {
+	unsigned short crc = games[i].crcCalculation(data);
+	
+	printf("Calculated CRC : 0x%p\n", crc);
+	
+	free(data);
+	
+	if(currentCrc != crc) {
 		if(askBeforeFixing) {
 			printf("Incorrect CRC, press enter to fix\n");
 			getchar();
 		}
 		
-		save.crc = crc;
-		
-		FILE *f = fopen(argv[1], "wb");
-		if(!f) {
-			printf("Failed to open\n");
-			fclose(f);
-			return 1;
-		}
-		
-		fwrite(&save, 1, sizeof(struct fifa08save), f);
-		fclose(f);
+		fseek(f, games[i].crcOffset, SEEK_SET);
+		fwrite(&crc, sizeof(unsigned short), 1, f);
 	}
 	
+	fclose(f);
 	return 0;
 }
